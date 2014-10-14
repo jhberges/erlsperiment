6175e104ceee82056fcb6e3ebfd34c6032a5a31a
jhberges <jhberges@gmail.com>
2014-10-14 18:15:49 +0200

    The experiments commence

--- cowboy_testing/src/cowboy_testing_handler.erl
+++ cowboy_testing/src/cowboy_testing_handler.erl
@@ -3,12 +3,22 @@
 -export([init/3]).
 -export([content_types_provided/2]).
 -export([get_html/2]).
+-export([get_json/2]).
 
 init(_, _Req, _Opts) ->
 	{upgrade, protocol, cowboy_rest}.
 
 content_types_provided(Req, State) ->
-	{[{{<<"text">>, <<"html">>, '*'}, get_html}], Req, State}.
+	{[{{<<"text">>, <<"html">>, '*'}, get_html},
+          {{<<"application">>, <<"json">>}, get_json}], Req, State}.
+
+get_json(Req, State) ->
+        case cowboy_req:binding(id, Req) of
+                undefined ->
+                        {<<"UNDEFINED?">>, Req, State};
+                {ItemId, Req2} ->
+                        {jiffy:encode({[{item, ItemId}]}), Req2, State}
+        end.
 
 get_html(Req, State) ->
-	{<<"<html><body>This is REST!</body></html>">>, Req, State}.
+	{<<"CHECK">>, Req, State}.	
