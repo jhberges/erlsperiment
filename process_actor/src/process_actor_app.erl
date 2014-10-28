6951ea6538a2cbeed47d17a7d11d0fda5e021831
jhberges <jhberges@gmail.com>
2014-10-28 17:14:24 +0100

    uploading

--- process_actor/src/process_actor_app.erl
+++ process_actor/src/process_actor_app.erl
@@ -7,7 +7,7 @@
 start(_Type, _Args) ->
 	ModulePid = spawn_link(process_actor_worker, work, []),
 	global:register_name(process_actor_worker_pid, ModulePid),
-	{ok, self()}.
+	process_actor_sup:start_link().
 
 stop(_State) ->
 	ok.
--- process_actor/src/process_actor_proc.erl
+++ process_actor/src/process_actor_proc.erl
@@ -1,5 +1,12 @@
 -module(process_actor_proc).
 -export([proc/0]).
+-export([proc_start_link/0]).
+
+proc_start_link() -> 
+	io:format("proc_start_link~n"),
+	ModulePid = spawn_link(process_actor_worker, work, []),
+	global:register_name(process_actor_worker_pid, ModulePid),
+	ModulePid.
 
 proc() ->
 	receive 
